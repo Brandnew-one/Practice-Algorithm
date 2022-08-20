e212bd7e6b83e651b39f6c1d6e08be3a46083698
Bran <cold929@naver.com>
2022-08-20 23:51:29 +0900

    add: 기타줄.swift

---
+++ boj/1049.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+// MARK: - 기타줄
+var input = readLine()!.split(separator: " ").map { Int(String($0))! }
+var n = input[0]
+let m = input[1]
+
+var priceSet: [Int] = []
+var priceOne: [Int] = []
+
+for _ in 0..<m {
+  input = readLine()!.split(separator: " ").map { Int(String($0))! }
+  priceSet.append(input[0])
+  priceOne.append(input[1])
+}
+
+priceSet.sort { $0 < $1 }
+priceOne.sort { $0 < $1 }
+
+var answer: Int = 0
+
+while n > 0 {
+  if n >= 6 {
+    if priceSet[0] > priceOne[0] * 6 {
+      answer += priceOne[0] * 6
+      n -= 6
+    } else {
+      answer += priceSet[0]
+      n -= 6
+    }
+  } else {
+    if priceOne[0] * n >= priceSet[0] {
+      answer += priceSet[0]
+      n -= 6
+    } else {
+      answer += priceOne[0] * n
+      n = 0
+    }
+  }
+}
+
+print(answer)
